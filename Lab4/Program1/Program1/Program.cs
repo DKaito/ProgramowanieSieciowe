2db20b2824789e1ba70a341d9201fc1c2c1e8b77
Bartlomiej Ch <202128@edu.p.lodz.pl>
2018-04-14 11:09:33 +0200

    Lab 4 - Program 1
    Dodanie Lab 4 - Program1,
    Podstawa do Lab 4 - Program2

---
+++ Lab4/Program1/Program1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread printingThread = new Thread(PrintHelloWorld);
+            printingThread.Start();
+            printingThread.Join();
+        }
+
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab4/Program2/Program2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Thread> threadList = new List<Thread>();
+            for (int i = 0; i < 10; i++)
+            {
+                Thread t = new Thread(printThread);
+                //t.Start();
+                threadList.Add(t);
+            }
+
+           /* foreach (var thread in threadList)
+            {
+                thread.Join();
+            }*/
+            while (true)
+            {
+                if (Console.ReadKey().Equals("q"))
+                    break;
+            }
+            Console.WriteLine("Quitted");
+            Console.ReadKey();
+        }
+
+        static void printThread()
+        {   
+            int number = Thread.CurrentThread.ManagedThreadId - 2;
+            if (number == 10)
+                number = 0;
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                Console.WriteLine(letter + "{0}", number);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
